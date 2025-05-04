9ed3d718975e947710f310c657fe8a71397b3e05
Owen Dirkes <odirkes@ucsd.edu>
2025-05-04 13:48:06 -0700

    Remove github pages specific settings for Vercel deployment

--- my-react-app/next.config.js
+++ my-react-app/next.config.js
@@ -1,8 +1,4 @@
 // filepath: /Users/owendirkes/Documents/React/my-react-app/next.config.js
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
-  output: 'export',
-  basePath: isProd ? '/my-react-workspace' : '',
-  assetPrefix: isProd ? '/my-react-workspace/' : '',
-};
+module.exports = {};
