f1cbec41c640fad1c492e070a0243f07b67bbcfd
Owen Dirkes <odirkes@ucsd.edu>
2025-05-06 13:02:17 -0700

    Removed basePath and assetPrefix in nextconfig.js

--- my-react-app/next.config.js
+++ my-react-app/next.config.js
@@ -1,7 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  basePath: isProd ? '/my-react-workspace' : '',
-  assetPrefix: isProd ? '/my-react-workspace/' : '',
   output: 'export',
 };
